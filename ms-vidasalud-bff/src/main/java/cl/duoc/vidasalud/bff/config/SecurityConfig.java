8babccc76f0d2d264e0a90bc76fe45590ac35acd
victor espindola <vict.espindola@duocuc.cl>
2026-09-15 22:12:23 -0300

    chore: preparar backend para despliegue EC2 y monitoreo

--- ms-vidasalud-bff/src/main/java/cl/duoc/vidasalud/bff/config/SecurityConfig.java
+++ ms-vidasalud-bff/src/main/java/cl/duoc/vidasalud/bff/config/SecurityConfig.java
@@ -16,6 +16,8 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
 
+            .cors(Customizer.withDefaults())
+
             .authorizeHttpRequests(auth -> auth
 
                 .requestMatchers("/api/bff/**")
